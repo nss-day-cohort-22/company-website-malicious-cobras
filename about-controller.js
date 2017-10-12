2db211b6727d384b5117cd943bd16deb11853c49
Deanna Vickers <deannavickers@Deannas-MacBook-Air.local>
2017-10-12 13:41:33 -0500

    fixed the locations loading issue

--- about-controller.js
+++ about-controller.js
@@ -1,14 +1,15 @@
-const store = JSON.parse(localStorage.getItem("storeLocations"))
+
+const companyDetail = JSON.parse(localStorage.getItem("storeLocations"))
 
 const storeInfo = document.getElementsByClassName("stores")[0]
 
-for (let key in store) {
-    const currentStore = store[key]
+for (let key in companyDetail) {
+    const currentStore = companyDetail[key]
 
     for (let i = 0; i < currentStore.length; i++) {
         let store = currentStore[i]
         storeInfo.innerHTML += `
-        <article class=$(key)>
+        <article class="$(key)">
             <h3>${store.name}</h3>
             <p>${store.city}</p>
             <p>${store.stateregion}</p>
