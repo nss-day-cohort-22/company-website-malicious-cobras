65c07f0e36f8449f31697e1ab25e6c0ed65a4be8
Deanna Vickers <deannavickers@Deannas-MacBook-Air.local>
2017-10-13 09:56:15 -0500

    clean up code

--- about-controller.js
+++ about-controller.js
@@ -1,16 +1,15 @@
-
 const companyDetail = JSON.parse(localStorage.getItem("storeLocations"))
 
 const storeInfo = document.getElementsByClassName("stores")[0]
 
-for (let key in companyDetail) 
+for (let key in companyDetail)
     {
     const currentStore = companyDetail[key]
 
-    for (let i = 0; i < currentStore.length; i++) 
+    for (let i = 0; i < currentStore.length; i++)
         {
         let store = currentStore[i]
-        storeInfo.innerHTML += 
+        storeInfo.innerHTML +=
         `
         <article class="$(key) gridbox">
             <h3>${store.name}</h3>
